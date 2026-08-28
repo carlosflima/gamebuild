b44dc54d2ff0be9fe2594f5996463626d24fe83c
carlosflima <44468589+carlosflima@users.noreply.github.com>
2026-08-28 16:49:22 -0300

    test: usar implementacao JVM de org.json

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    testImplementation("org.json:json:20260814")
 }
