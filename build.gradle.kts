bcb716bda787eef9d2ca61d05c7496651b26d03b
carlosflima <44468589+carlosflima@users.noreply.github.com>
2026-08-28 17:07:13 -0300

    build: migrar toolchain para AGP 9 e Kotlin integrado

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+buildscript {
+    dependencies {
+        // AGP 9 usa Kotlin integrado; esta versão também é usada pelo Compose Compiler.
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.android.application") version "9.3.2" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
