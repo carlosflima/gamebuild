794eacaaa49dd5f7b14e06f423dbdd53f81b220c
carlosflima <44468589+carlosflima@users.noreply.github.com>
2026-09-10 16:36:27 -0300

    feat: iniciar catálogo de personagens Endfield

---
+++ app/src/main/java/com/carlosflima/gamebuild/data/EndfieldCharacterCatalog.kt
@@ -0,0 +1,7 @@
+package com.carlosflima.gamebuild.data
+
+import com.carlosflima.gamebuild.domain.GameCharacter
+
+internal object EndfieldCharacterCatalog {
+    val characters: List<GameCharacter> = emptyList()
+}
