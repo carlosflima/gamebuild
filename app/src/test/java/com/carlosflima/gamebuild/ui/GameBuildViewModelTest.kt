c1085a83b1a8a9307795ee3616fca245d46959c5
carlosflima <44468589+carlosflima@users.noreply.github.com>
2026-09-14 17:01:07 -0300

    test: validar navegação da build de Camille

--- app/src/test/java/com/carlosflima/gamebuild/ui/GameBuildViewModelTest.kt
+++ app/src/test/java/com/carlosflima/gamebuild/ui/GameBuildViewModelTest.kt
@@ -41,7 +41,7 @@ class GameBuildViewModelTest {
     }
 
     @Test
-    fun defaultRepositoryLoadsEndfieldRosterAndMiFuBuild() {
+    fun defaultRepositoryLoadsEndfieldRosterAndCamilleBuild() {
         val viewModel = GameBuildViewModel()
 
         viewModel.selectGame(Game.ENDFIELD)
@@ -49,18 +49,18 @@ class GameBuildViewModelTest {
         val rosterState = viewModel.uiState.value
         assertEquals(Game.ENDFIELD, rosterState.selectedGame)
         assertEquals(
-            listOf("Endministrator", "Perlica", "Chen Qianyu", "Wulfgard", "Yvonne", "Typhoeus", "Antal", "Akekuri", "Avywenna", "Catcher", "Alesh", "Xaihi", "Snowshine", "Arclight", "Estella", "Fluorite", "Ardelia", "Ember", "Lifeng", "Pogranichnik", "Last Rite", "Gilberta", "Liino", "Tangtang", "Laevatain", "Zhuang Fangyi", "Da Pan", "Mi Fu"),
+            listOf("Endministrator", "Perlica", "Chen Qianyu", "Wulfgard", "Yvonne", "Typhoeus", "Antal", "Akekuri", "Avywenna", "Catcher", "Alesh", "Xaihi", "Snowshine", "Arclight", "Estella", "Fluorite", "Ardelia", "Ember", "Lifeng", "Pogranichnik", "Last Rite", "Gilberta", "Liino", "Tangtang", "Laevatain", "Zhuang Fangyi", "Da Pan", "Mi Fu", "Camille"),
             rosterState.characters.map { it.name }
         )
         assertEquals(setOf(Game.ENDFIELD), rosterState.characters.map { it.game }.toSet())
 
-        val miFu = rosterState.characters.first { it.name == "Mi Fu" }
-        viewModel.selectCharacter(miFu)
+        val camille = rosterState.characters.first { it.name == "Camille" }
+        viewModel.selectCharacter(camille)
 
         val buildState = viewModel.uiState.value
-        assertEquals("Mi Fu", buildState.selectedCharacter?.name)
+        assertEquals("Camille", buildState.selectedCharacter?.name)
         assertEquals(
-            listOf("endfield-mi-fu-f2p-starter-2026-09"),
+            listOf("endfield-camille-f2p-starter-2026-09"),
             buildState.builds.map { it.id }
         )
         assertEquals(BuildType.F2P, buildState.builds.single().type)
